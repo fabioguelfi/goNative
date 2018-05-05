72f00233688df108d2d3745beccc929be79d2894
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-05 15:31:16 -0300

    Navegação Welcome

--- appGitHubExplorer/src/pages/welcome/index.js
+++ appGitHubExplorer/src/pages/welcome/index.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles';
 
 import { View, Text, TextInput, TouchableOpacity, StatusBar } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import PropTypes from 'prop-types';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Bem-Vindo</Text>
-    <Text style={styles.text}>Para continuar, precisamos que informe seu usuario do Github</Text>
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        autoCorrect={false}
-        placeholder="Digite seu usuario"
-        underlineColorAndroid="rgba(0,0,0,0)">
-      </TextInput>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => { }}>  
-        <Text style={styles.buttonText}>Prosseguir</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+export default class Welcome extends Component {
 
-Welcome.navigationOptions = { 
-  header: null,
+  static navigationOptions = {
+    header: null,
+  }
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  }
+
+  signIn = () => {
+    console.log('call')
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
+
+    this.props.navigation.dispatch(resetAction);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Bem-Vindo</Text>
+        <Text style={styles.text}>Para continuar, precisamos que informe seu usuario do Github</Text>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuario"
+            underlineColorAndroid="rgba(0,0,0,0)">
+          </TextInput>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.signIn}>
+            <Text style={styles.buttonText}>Prosseguir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 }
-
-StatusBar.setBarStyle("light-content")
-
-export default Welcome;
