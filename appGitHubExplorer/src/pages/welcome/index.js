edfd74df0431223ffdb34088cfe99df61ccb7b68
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-05 14:21:00 -0300

    estilizando welcome

--- appGitHubExplorer/src/pages/welcome/index.js
+++ appGitHubExplorer/src/pages/welcome/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './styles';
 
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-const Welcome = () => <View />;
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Bem-Vindo</Text>
+    <Text style={styles.text}>Para continuar, precisamos que informe seu usuario do Github</Text>
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuario"
+        underlineColorAndroid="rgba(0,0,0,0)">
+      </TextInput>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => { }}>  
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+Welcome.navigationOptions = { 
+  header: null,
+}
 
 export default Welcome;
