da79a09d75cd495678c2afe989d90f9c4bde614e
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-05 23:50:01 -0300

    Consumindo orgs da API

--- appGitHubExplorer/src/pages/organizations/index.js
+++ appGitHubExplorer/src/pages/organizations/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { View, AsyncStorage, ActivityIndicator, Text, FlatList } from 'react-native';
+import api from '../../services/api';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { View } from 'react-native';
+import styles from './style';
 
 export default class Organizations extends Component {
 
@@ -9,7 +11,51 @@ export default class Organizations extends Component {
     tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor}/>
   };
 
-  render() {
-    return <View />
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
   }
+
+  componentDidMount() {
+    this.loadOrganizations();
+  }
+
+  loadOrganizations = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    const response = await api.get(`/users/${username}/orgs`);
+
+    this.setState({
+      data: response.data,
+      loading: false,
+      refreshing: false,
+    });
+  };
+
+  renderListItem = ({ item }) => <Text>{ item.login }</Text>
+
+  renderList = () => (
+    <FlatList
+      data={this.state.data}
+      keyExtractor={item => String(item.id)}
+      renderItem={this.renderListItem}
+      onRefresh={this.loadOrganizations}
+      refreshing={this.state.refreshing}
+      ListFooterComponent={<View style={styles.listFooter} />}
+    />
+  );
+
+  render() {
+    return (
+      <View>
+        { this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList() }
+      </View>
+    );
+  }
+
 }
