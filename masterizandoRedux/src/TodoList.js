e6d8324335a45c808a7f7ea15a31dfbca02ac562
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-06 11:33:09 -0300

    cadastro novos todos

--- masterizandoRedux/src/TodoList.js
+++ masterizandoRedux/src/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from 'store/actions/todos';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,18 +12,23 @@ const styles = StyleSheet.create({
   }
 })
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, addTodo }) => {
     console.tron.log(todos)
     return (
       <View style={styles.container}>
         { todos.map(item => <Text>{ item }</Text>) }
+        <TouchableOpacity onPress={ () => addTodo('Fazer Cafe Novamente') }>
+          <Text>Adicionar Todo</Text>
+        </TouchableOpacity>
       </View>
     );
   };
 
-
 const mapStateToProps = state => ({
   todos: state.todos,
-})
+});
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => bindActionCreators(TodoActions, dispatch);
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
