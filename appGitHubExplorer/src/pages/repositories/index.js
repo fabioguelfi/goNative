56c48de3f1f3f1c0b9f2e5c9aff406b630da4bb4
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-05 23:41:12 -0300

    Estilizando repos

--- appGitHubExplorer/src/pages/repositories/index.js
+++ appGitHubExplorer/src/pages/repositories/index.js
@@ -3,55 +3,62 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { View, AsyncStorage, ActivityIndicator, FlatList, Text } from 'react-native';
 import api from '../../services/api';
 import styles from './style';
+import RepositoryItem from './componnets';
 
-export default class Repositorie extends Component {
 
-  state = {
-    data: [],
-    loading: true,
-  }
+
+export default class Repositories extends Component {
 
   static navigationOptions = {
     title: 'Repositórios',
     tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
   };
 
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  }
+
   componentDidMount() {
     this.loadRepositories();
   }
 
   loadRepositories = async () => {
+    this.setState({ refreshing: true });
+
     const username = await AsyncStorage.getItem('@Githuber:username');
+
     const response = await api.get(`/users/${username}/repos`);
 
     this.setState({
       data: response.data,
-      loading: false
+      loading: false,
+      refreshing: false,
     });
-
   };
 
-  renderListItem = ({ item }) => <Text>{item.full_name}</Text>
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />
 
-  renderList = () => {
+  renderList = () => (
     <FlatList
       data={this.state.data}
       keyExtractor={item => String(item.id)}
       renderItem={this.renderListItem}
+      onRefresh={this.loadRepositories}
+      refreshing={this.state.refreshing}
+      ListFooterComponent={<View style={styles.listFooter} />}
     />
-  };
+  );
 
   render() {
     return (
-      <View style={styles.container}>
-        {
-          this.state.loading
-            ? <ActivityIndicator style={styles.loading} />
-            : this.renderList()
-        }
+      <View>
+        { this.state.loading
+          ? <ActivityIndicator style={styles.loading} />
+          : this.renderList() }
       </View>
     );
   }
-
 }
 
