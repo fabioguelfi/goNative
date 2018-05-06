003792f84bf51aa1630711d9654028ac32256641
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-06 14:39:47 -0300

    alterando todos para objetos

--- masterizandoRedux/src/TodoList.js
+++ masterizandoRedux/src/TodoList.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as TodoActions from 'store/actions/todos';
+import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,7 +17,7 @@ const TodoList = ({ todos, addTodo }) => {
     console.tron.log(todos)
     return (
       <View style={styles.container}>
-        { todos.map(item => <Text>{ item }</Text>) }
+        { todos.map(item => <Text key={item.id}>{ item.text }</Text>) }
         <TouchableOpacity onPress={ () => addTodo('Fazer Cafe Novamente') }>
           <Text>Adicionar Todo</Text>
         </TouchableOpacity>
@@ -24,11 +25,18 @@ const TodoList = ({ todos, addTodo }) => {
     );
   };
 
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string,
+  })).isRequired,
+  addTodo: PropTypes.func.isRequired,
+}
+
 const mapStateToProps = state => ({
   todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(TodoActions, dispatch);
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- masterizandoRedux/src/store/reducers/todos.js
+++ masterizandoRedux/src/store/reducers/todos.js
@@ -1,12 +1,12 @@
 const initialState = [
-  'Fazer Café',
-  'Estudar Gonative'
+  { id: 0, text: 'Fazer Café'},
+  { id: 1, text: 'Estudar Gonative'},
 ]
 
 export default function todo(state = initialState, action) {
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state, action.text];
+      return [...state, { id: Math.random(), text: action.payload.text }];
     default:
       return state;
   }
