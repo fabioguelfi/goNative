304e0e9c6480892b0f2d852d18b6ecbfb239990a
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-05-06 15:10:42 -0300

    configurando novo app

--- MasterizandoRedux/src/index.js
+++ MasterizandoRedux/src/index.js
@@ -3,13 +3,12 @@ import { View } from 'react-native';
 import ReactotronConfig from 'config/ReactotronConfig';
 import { Provider } from 'react-redux';
 import store from 'store';
-import TodoList from './TodoList';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <TodoList />
+      <View></View>
       </Provider>
     );
   }
